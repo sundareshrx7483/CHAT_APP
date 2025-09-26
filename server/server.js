887cd5ec731b0e7472e71d2212c24a7e76c108a7
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-09-26 23:41:06 +0530

    cleared dotenv from server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
