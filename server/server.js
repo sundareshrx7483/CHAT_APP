60ba513039f752d910a4fe327de78a504a13c245
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-09-27 11:46:15 +0530

    added socket.io

--- server/server.js
+++ server/server.js
@@ -9,10 +9,20 @@ import chatRoutes from "./routes/chatRoute.js";
 import cookieParser from "cookie-parser";
 import config from "./config/config.js";
 import morgan from "morgan";
+import http from "http";
+import { Server } from "socket.io";
 
 const { port, mongoUri, jwtSecret } = config;
 
 export const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -25,11 +35,35 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const userSocketMap = new Map();
+
+io.on("connection", (socket) => {
+  socket.on("join", (userId) => {
+    userSocketMap.set(userId, socket.id);
+  });
+
+  socket.on("sendMessage", async (data) => {
+    const receiverSocketId = userSocketMap.get(data.receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("receiveMessage", data);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const userId = [...userSocketMap.entries()].find(
+      ([_, sId]) => sId === socket.id
+    )?.[0];
+    if (userId) {
+      userSocketMap.delete(userId);
+    }
+  });
+});
+
 try {
   const dbConnection = await mongoose.connect(mongoUri);
   console.log("Connected to MongoDB");
   if (dbConnection) {
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`Server started on ${port}`);
     });
   }
